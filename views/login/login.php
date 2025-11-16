e9e7b8b7e0e9ce9e89adf47588e8a73d4842d75e
Huyen318006 <ngochuyen318006@gmail.com>
2025-11-16 17:04:25 +0700

    tắt bootraps

--- views/login/login.php
+++ views/login/login.php
@@ -75,21 +75,6 @@
     </div>
   </div>
 
-  <script>
-    function togglePassword() {
-      const p = document.getElementById('password');
-      const i = document.getElementById('eyeIcon');
-      if (p.type === 'password') {
-        p.type = 'text';
-        i.classList.replace('fa-eye', 'fa-eye-slash');
-      } else {
-        p.type = 'password';
-        i.classList.replace('fa-eye-slash', 'fa-eye');
-      }
-    }
-  </script>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
+  
 </body>
 </html>
