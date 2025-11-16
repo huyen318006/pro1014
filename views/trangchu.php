dc9361e4bdd58dd7e3bb29add241ce0b2d9065eb
Huyen318006 <ngochuyen318006@gmail.com>
2025-11-16 18:25:48 +0700

    sửa lại link asset

--- views/trangchu.php
+++ views/trangchu.php
@@ -9,7 +9,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 
-  <link rel="stylesheet" href="../assets/css/trangchu.css">
+  <link rel="stylesheet" href="../asset/css/trangchu.css">
   <!-- Custom CSS -->
 
 </head>
