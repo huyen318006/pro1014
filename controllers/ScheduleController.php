9185be01cc12d65ae0f7e21a5c54a0943d484a1a
Baisery <mgbaiser@gmail.com>
2025-11-17 20:04:59 +0700

    Thêm chức năng quản lý lịch trình: thêm form thêm, chỉnh sửa và xóa lịch trình; cập nhật xử lý upload ảnh

--- controllers/ScheduleController.php
+++ controllers/ScheduleController.php
@@ -14,4 +14,38 @@ class ScheduleController
         $schedules = $this->modelSchedule->getAllSchedules();
         require_once BASE_URL_VIEWS . 'admin/schedule/list.php';
     }
+    public function addScheduleForm()
+    {
+        require_once BASE_URL_VIEWS . 'admin/schedule/add.php';
+    }
+    public function store()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = $_POST;
+            
+            // Xử lý upload ảnh
+            if (isset($_FILES['urlSP']) && $_FILES['urlSP']['size'] > 0) {
+                $data['urlSP'] = upload_file('product', $_FILES['urlSP']);
+            } else {
+                $data['urlSP'] = 'product/default.jpg';
+            }
+            
+            $this->product->insert($data);
+        }
+        header("Location:" . BASE_URL);
+    }
+    public function editScheduleForm()
+    {
+        if (isset($_GET['id'])) {
+            $schedule = $this->modelSchedule->getScheduleById($_GET['id']);
+            require_once BASE_URL_VIEWS . 'admin/schedule/edit.php';
+        }
+    }
+    function delete()
+    {
+        if (isset($_GET['id'])) {
+            $this->modelSchedule->deleteSchedule($_GET['id']);
+        }
+        header("Location:" . BASE_URL . "?act=listSchedule");
+    }
 }
