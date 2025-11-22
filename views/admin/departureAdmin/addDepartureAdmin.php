ac0b5124a4f03b0bd0f794a60016ea4b583962c1
Baisery <mgbaiser@gmail.com>
2025-11-22 14:17:55 +0700

    fix: cập nhật liên kết quản lý lịch trình trong các trang admin

--- views/admin/departureAdmin/addDepartureAdmin.php
+++ views/admin/departureAdmin/addDepartureAdmin.php
@@ -24,7 +24,7 @@
         <a href="index.php?act=home" class="active"><i class="fas fa-tachometer-alt"></i> <span>Dashboard</span></a>
         <a href="<?= BASE_URL . '?act=account' ?>"><i class="fas fa-users-cog"></i> <span>Quản lý tài khoản</span></a>
         <a href="index.php?act=listTours"><i class="fas fa-map-marked-alt"></i> <span>Quản lý Tour</span></a>
-        <a href="index.php?act=listSchedule"><i class="fas fa-map-marked-alt"></i> <span>Quản lý Lịch Trình</span></a>
+        <a href="index.php?act=listItinerary"><i class="fas fa-map-marked-alt"></i> <span>Quản lý Lịch Trình</span></a>
         <a href="?act=listAssignments"><i class="fas fa-map-marked-alt"></i> <span>Phân công HDV</span></a>
         <a href="index.php?act=services"><i class="fas fa-concierge-bell"></i> <span>Quản lý Dịch Vụ</span></a>
         <a href="#"><i class="fas fa-shopping-cart"></i> <span>Quản lý đơn đặt</span></a>
